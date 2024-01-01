675794e9299b6460bc6f77f08f18efacd0e60eaa
HalaTheAnalyst <140041037+hal00sh@users.noreply.github.com>
2024-01-01 22:16:25 +0300

    Add files via upload

---
+++ SQLQuery9.sql
@@ -0,0 +1,31 @@
+Table 1 Query:
+Create Table employees_table 
+(EmployeeID int, 
+FirstName varchar(50), 
+Age int, 
+)
+
+Table 2 Query:
+Create Table employeesalary 
+(EmployeeID int, 
+JobTitle varchar(50), 
+SalaryThisMonth int
+)
+
+
+Table 1 Insert:
+Insert into employees_table VALUES
+(1, 'Jim', 30),
+(2, 'Pam', 30),
+(3, 'Dwight', 29),
+(4, 'Angela', 31),
+(5, 'Toby', 32)
+
+
+Table 2 Insert:
+Insert Into employeesalary VALUES
+(1, 'Salesman', 16000),
+(2, 'Receptionist', 11000),
+(3, 'Salesman', 17000),
+(4, 'Accountant', 14000),
+(5, 'HR', 15000)
